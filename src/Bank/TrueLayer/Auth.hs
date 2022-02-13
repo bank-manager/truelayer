2aef3e3993a6809735ee4bbe74cbc09eaeb85c1d
Riley Evans <riley@rileyevans.co.uk>
2022-02-13 00:27:08 +0000

    Update OAuth dependency & add codeSwap

--- src/Bank/TrueLayer/Auth.hs
+++ src/Bank/TrueLayer/Auth.hs
@@ -2,29 +2,37 @@
 {-# LANGUAGE QuasiQuotes       #-}
 module Bank.TrueLayer.Auth
   ( genAccessToken
+  , swapCode
   ) where
 
 import           Network.HTTP.Client     (newManager)
 import           Network.HTTP.Client.TLS (tlsManagerSettings)
 import           Network.OAuth.OAuth2
-    (ExchangeToken, OAuth2 (..), OAuth2Token, RefreshToken, refreshAccessToken)
+    (ExchangeToken, OAuth2 (..), OAuth2Token, RefreshToken, fetchAccessToken, refreshAccessToken)
 import           URI.ByteString.QQ       (uri)
 
 oauthSettings :: OAuth2
 oauthSettings = OAuth2
-  { oauthClientId = "bankmanager-772883"
-  , oauthClientSecret = Just ""
-  , oauthOAuthorizeEndpoint = [uri|https://auth.truelayer.com/connect/token|]
-  , oauthAccessTokenEndpoint = [uri|https://auth.truelayer.com/connect/token|]
-  , oauthCallback = Just [uri|https://console.truelayer.com/redirect-page|]
+  { oauth2ClientId = "bankmanager-772883"
+  , oauth2ClientSecret = Just ""
+  , oauth2AuthorizeEndpoint = [uri|https://auth.truelayer.com/connect/token|]
+  , oauth2TokenEndpoint = [uri|https://auth.truelayer.com/connect/token|]
+  , oauth2RedirectUri = Just [uri|https://console.truelayer.com/redirect-page|]
   }
 
 genAccessToken :: RefreshToken -> IO (Maybe OAuth2Token)
 genAccessToken refreshToken = do
   manager <- newManager tlsManagerSettings
   token <- refreshAccessToken manager oauthSettings refreshToken
-  return (
-    case token of
-      Left err    -> Nothing
-      Right token -> Just token
-    )
+  return $ case token of
+    Left err    -> Nothing
+    Right token -> Just token
+
+
+swapCode :: ExchangeToken -> IO (Maybe OAuth2Token)
+swapCode code = do
+  manager <- newManager tlsManagerSettings
+  token <- fetchAccessToken manager  oauthSettings code
+  return $ case token of
+    Left err    -> Nothing
+    Right token -> Just token
